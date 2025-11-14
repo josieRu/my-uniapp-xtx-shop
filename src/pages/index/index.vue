8b99b1ec55e054e445133c00f7a7e901d8757be5
josie <josieru@qq.com>
2025-11-14 15:16:14 +0800

    feat(index): 添加猜你喜欢组件并优化页面滚动布局
    在首页中引入了 XtxGuess 组件，用于展示猜你喜欢的内容。
    同时使用 scroll-view 包裹内容区域以支持垂直滚动，并调整样式使页面
    结构更合理，提升用户体验。
    此外，在类型声明文件中注册了新增的全局组件 XtxGuess。

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,21 +2,32 @@
 import CustomNavbar from './components/CustomNavbar.vue'
 import CategoryPanel from './components/CategoryPanel.vue'
 import HotPanel from './components/HotPanel.vue'
+import XtxGuess from '@/components/XtxGuess.vue'
 </script>
 
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavbar />
-  <!-- 轮播图 -->
-  <XtxSwiper />
-  <!-- 分类面板 -->
-  <CategoryPanel />
-  <!-- 热门面板 -->
-  <HotPanel />
+  <scroll-view class="scroll-view" scroll-y>
+    <!-- 轮播图 -->
+    <XtxSwiper />
+    <!-- 分类面板 -->
+    <CategoryPanel />
+    <!-- 热门面板 -->
+    <HotPanel />
+    <!-- 猜你喜欢 -->
+    <XtxGuess />
+  </scroll-view>
 </template>
 
 <style lang="scss">
 page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
 }
 </style>
--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -5,8 +5,11 @@
  */
 import 'vue'
 import XtxSwiper from './XtxSwiper.vue'
+import XtxGuess from './XtxGuess.vue'
+
 declare module 'vue' {
   export interface GlobalComponents {
     XtxSwiper: typeof XtxSwiper
+    XtxGuess: typeof XtxGuess
   }
 }
